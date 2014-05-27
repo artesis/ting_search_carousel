c77b71b19b882e9b4882a529c77d3f891b123063
Max Melnic <mm@inleadmedia.com>
2014-05-27 11:33:02 +0300

    Case 4161 - Remove author from info bloc, truncate title to 25 symbols

--- ting_search_carousel_collection.tpl.php
+++ ting_search_carousel_collection.tpl.php
@@ -8,8 +8,7 @@
   <a href="search/ting/<?php echo $collection->id; ?>" title="<?php print check_plain($collection->creator); ?>: <?php print check_plain($collection->title); ?>">
     <img src="<?php echo $collection->image; ?>" alt=""/>
     <div class="info">
-      <span class="creator"><?php print check_plain($collection->creator); ?></span>
-      <span class="title"><?php print check_plain($collection->title); ?></span>
+      <span class="title"><?php print truncate_utf8(check_plain($collection->title), 25, FALSE, TRUE); ?></span>
     </div>
   </a>
 </li>
